17dac5031c6291fb810c99e7a488e388a6b885dc
Vivek Myers <vivek.myers@gmail.com>
2019-06-02 00:44:12 -0700

    benchmarking

---
+++ Binomial.hs
@@ -0,0 +1,108 @@
+module Binomial (
+    PriorityQueue, new, add, poll, merge, decrease, addAll, toList, isEmpty
+    ) where
+
+-- |Contains binomial queue of added elements and binomial queue of deleted ones
+data PriorityQueue e a = PQ {added :: [Tree e a], deleted :: [Tree e a]} deriving Show
+
+-- |Constructs an empty priority queue with elements e and ordering a in O(1) time
+new :: (Eq e, Ord a) => PriorityQueue e a
+new = PQ [] []
+
+-- |Gets if the queue is empty in O(1) time
+isEmpty :: (Eq e, Ord a) => PriorityQueue e a -> Bool
+isEmpty PQ {added = []} = True
+isEmpty _ = False 
+
+-- |Add x with priority p to pq in O(1) time
+add :: (Eq e, Ord a) => e -> a -> PriorityQueue e a -> PriorityQueue e a
+add x p pq = pq {added = insert x p $ added pq}
+
+{-
+    |Return the minimum element and the remaining queue after 
+polling in O(log n) amortized time. The operation is O(1) non-amortized time if 
+only the first element is used, as the removal of the element will not be evaluated
+(it becomes equivalent to find-min).
+-}
+poll :: (Eq e, Ord a) => PriorityQueue e a -> (e, PriorityQueue e a)
+poll PQ {added = []} = empty
+poll pq = (first, cleanup . PQ rest $ deleted pq)
+    where first = fst . findMin . added $ pq
+          rest = deleteMin . added $ pq
+
+-- |Meld two priority queues in O(1)
+merge :: (Eq e, Ord a) => PriorityQueue e a -> PriorityQueue e a -> PriorityQueue e a
+merge a b = PQ (meld (added a) (added b)) (meld (deleted a) (deleted b))
+
+-- |Decrease the priority of x from p to q in pq in O(1) amortized
+decrease :: (Eq e, Ord a) => e -> a -> a -> PriorityQueue e a -> PriorityQueue e a 
+decrease x p q pq
+    | q < p = cleanup $ PQ (insert x q $ added pq) (insert x p $ deleted pq)
+    | otherwise = error "must decrease key"
+
+-- |Adds a list of m (element, priority) pairs to the given priority queue in O(m) time
+addAll :: (Eq e, Ord a) => [(e, a)] -> PriorityQueue e a -> PriorityQueue e a
+addAll list pq = foldr (uncurry add) pq list
+
+-- |Converts the priority queue to a sorted list in O(n) time
+toList :: (Eq e, Ord a) => PriorityQueue e a -> [e]
+toList pq
+    | isEmpty pq = []
+    | otherwise = x : toList rest
+    where (x, rest) = poll pq
+
+-- |Removes tombstones
+cleanup pq@PQ {added = []} = pq
+cleanup pq@PQ {deleted = []} = pq
+cleanup pq@PQ {added = a, deleted = d}
+    | findMin a == findMin d = cleanup $ PQ (deleteMin a) (deleteMin d)
+    | otherwise = pq
+
+
+-- |Binomial tree of nodes with root, priority, and tree rank
+data Tree e a = Node {
+    root :: e, 
+    priority :: a, 
+    rank :: Integer, 
+    children :: [Tree e a]
+    } deriving Show
+
+-- helper functions for operations on lists of skew binomial trees below
+
+link t1@(Node e1 a1 r1 c1) t2@(Node e2 a2 r2 c2)
+    | a1 > a2 = Node e2 a2 (r2 + 1) $ t1 : c2
+    | otherwise = Node e1 a1 (r1 + 1) $ t2 : c1
+
+ins t [] = [t]
+ins t (t':ts)
+    | rank t < rank t' = t : t' : ts
+    | otherwise = flip ins ts $ link t t'
+
+insert x p ts = ins (Node x p 0 []) ts
+
+meld [] ts = ts
+meld ts [] = ts
+meld (t1:ts1) (t2:ts2)
+    | rank t1 < rank t2 = t1 : meld ts1 (t2 : ts2)
+    | rank t2 < rank t1 = t2 : meld (t1 : ts1) ts2
+    | otherwise = ins (link t1 t2) (meld ts1 ts2)
+
+empty = error "empty queue"
+
+findMin [] = empty
+findMin [t] = (root t, priority t)
+findMin (t:ts)
+    | priority t <= p = (root t, priority t)
+    | otherwise = (x, p)
+    where (x, p) = findMin ts
+    
+deleteMin [] = empty
+deleteMin ts = flip meld ts'' $ reverse c
+    where 
+        getMin [t] = (t, [])
+        getMin (t:ts)
+            | priority t <= priority t' = (t, ts)
+            | otherwise = (t', t:ts')
+            where (t', ts') = getMin ts
+        (Node x p r c, ts'') = getMin ts
+
---
+++ bench.hs
@@ -0,0 +1,69 @@
+import qualified Data.Map as Map
+import Data.Map (Map, (!))
+import qualified Data.Set as Set
+import Data.Set (Set)
+import PriorityQueue
+import qualified Binomial as B
+import System.Random
+import Criterion.Main
+
+skewHeapsort xs = toList $ addAll (zip xs xs) new
+binHeapsort xs = B.toList $ B.addAll (zip xs xs) B.new
+
+skewMeld nums = toList . foldr f new $ 
+                    do num <- chunk (2 * n) nums 
+                       let queue = addAll (zip <*> id $ num) new
+                       return queue
+    where n = floor $ fromIntegral (length nums) / 100
+          f pq1 pq2 = iterate (snd.poll) (merge pq1 pq2) !! n
+binMeld nums = B.toList . foldr f B.new $ 
+                    do num <- chunk (2 * n) nums 
+                       let queue = B.addAll (zip <*> id $ num) B.new
+                       return queue
+    where n = floor $ fromIntegral (length nums) / 100
+          f pq1 pq2 = iterate (snd.B.poll) (B.merge pq1 pq2) !! n
+
+skewDijkstra :: [Integer] -> Int -> [Integer] 
+skewDijkstra nums deg = toList . fst $ foldr f (addAll [(x, m) | x <- nums] new, 
+        Map.fromList [(i, m) | i <- nums]) (chunk deg nums)
+    where f near (pq, weights) = (foldr (\x q -> let i = nw ! x
+                                                 in if i < 0 then q
+                                                    else decrease x i (i - 1) q) rest near, 
+                    foldr (flip (Map.insertWith (+)) (-1)) nw near)        
+                where (popped, rest) = poll pq
+                      nw = Map.insert popped (-1) weights    
+          m = floor . exp $ 9 * log 10
+binDijkstra :: [Integer] -> Int -> [Integer] 
+binDijkstra nums deg = B.toList . fst $ foldr f (B.addAll [(x, m) | x <- nums] B.new, 
+        Map.fromList [(i, m) | i <- nums]) (chunk deg nums)
+    where f near (pq, weights) = (foldr (\x q -> let i = nw ! x
+                                                 in if i < 0 then q
+                                                    else B.decrease x i (i - 1) q) rest near, 
+                    foldr (flip (Map.insertWith (+)) (-1)) nw near)        
+                where (popped, rest) = B.poll pq
+                      nw = Map.insert popped (-1) weights    
+          m = floor . exp $ 9 * log 10
+
+
+chunk n [] = []
+chunk n xs = take n xs : chunk n (drop n xs)
+
+run n = do
+    let max = floor . exp $ n * log 10 :: Integer
+    nums <- take (fromIntegral max) <$> (randomRs (1, 1000 * max) <$> getStdGen) :: IO [Integer]
+    let benchn x = bench $ x ++ "-" ++ show max
+    defaultMain [ 
+            bgroup "binomial" [ 
+                benchn "heapsort" $ whnf binHeapsort nums,
+                benchn "meld" $ whnf binMeld nums,
+                benchn "dijkstra" $ whnf (binDijkstra nums) 4
+                ],
+            bgroup "skew" [ 
+                benchn "heapsort" $ whnf skewHeapsort nums,
+                benchn "meld" $ whnf skewMeld nums,
+                benchn "dijkstra" $ whnf (skewDijkstra nums) 4
+                ]
+        ]
+
+main = do
+    mapM_ run [4, 5, 6, 7]
